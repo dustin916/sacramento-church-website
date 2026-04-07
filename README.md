344c3105684bf60114d5287b9a468d293d27ede7
dustin916 <dustin.tech22@gmail.com>
2026-04-06 23:28:58 -0400

    Changed sensitive info (phone number and email)

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
  Church Website
  A Church website I built for the church I pastored at the time.
  Tools used were Django, Python, HTML, CSS and some JavaScript
+
+
+ Church decided to close website in June of 2024. 
--- SacramentoWMC/SacramentoWMC/settings.py
+++ SacramentoWMC/SacramentoWMC/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Email Settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sacramentowmc@gmail.com'
+EMAIL_HOST_USER = 'xxxxxxx@gmail.com'
 EMAIL_HOST_PASSWORD = 'dyle qlqq jydv krnf'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
